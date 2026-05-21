464f66191a2c05f68ebc3af5bc2571ffa775f46f
mutoniscoviadev <mutoniscovia22@gmail.com>
2026-05-21 11:24:33 +0200

    fix: add /api/v1 prefix to swagger server URL

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -12,7 +12,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: process.env["API_URL"] || "http://localhost:3000",
+        url: `${process.env["API_URL"] || "http://localhost:3000"}/api/v1`,
         description: "API Server",
       },
     ],
